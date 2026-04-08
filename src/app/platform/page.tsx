37628ac4d1e888b57f7badc6c8878498f9ad3dba
sanketparajuli <parajulisanket7@gmail.com>
2026-04-08 10:59:36 +0545

    chnages in add

--- src/app/platform/page.tsx
+++ src/app/platform/page.tsx
@@ -194,7 +194,7 @@ export default function PlatformPage() {
                     display: "inline-block",
                   }}
                 >
-                  About Quantum Link
+                  About Quantum Link cfghvjb
                 </motion.p>
 
                 <span className="animate-line-side delay-animation" />
